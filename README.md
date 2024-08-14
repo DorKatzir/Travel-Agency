b1d82d7ff2286b6156022e6c465c36a084489076
Dor Katzir <katzirdor@gmail.com>
2024-08-14 22:09:49 +0300

    Section 4 - Admin Panel Setup - Template Mastering

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 
 #### Section 4 - Admin Panel Setup - Html Template
+#### Section 4 - Admin Panel Setup - Template Mastering
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminLoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+// Admin
+Route::prefix('admin')->group( function () {
+    Route::get('/login', [AdminLoginController::class, 'login'])->name('admin_login');
+
+});
+
+
