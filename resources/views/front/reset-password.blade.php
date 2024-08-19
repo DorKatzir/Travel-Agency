5877239065864885590b9f5b1523a7da0aaa7967
Dor Katzir <katzirdor@gmail.com>
2024-08-19 11:09:15 +0300

    Retype

--- resources/views/front/reset-password.blade.php
+++ resources/views/front/reset-password.blade.php
@@ -33,7 +33,7 @@
                             </div>
 
                             <div class="mb-3">
-                                <label for="confirm_password" class="form-label">Confirm Password</label>
+                                <label for="confirm_password" class="form-label">Retype Password</label>
                                 <input type="password" class="form-control" name="confirm_password">
                             </div>
 
