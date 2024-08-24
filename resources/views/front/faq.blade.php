af4e0459c6a8ae65d953d04c91c60e29755180e0
Dor Katzir <katzirdor@gmail.com>
2024-08-24 17:18:41 +0300

    FAQ loop itteration

--- resources/views/front/faq.blade.php
+++ resources/views/front/faq.blade.php
@@ -26,16 +26,26 @@
                     <div class="accordion" id="accordionExample">
                         @foreach ($faqs as $faq)
                             <div class="accordion-item mb_30">
-                                <h2 class="accordion-header" id="{{ 'heading_'.$faq->id }}">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="{{ '#collapse_'.$faq->id }}" aria-expanded="false" aria-controls="{{ 'collapse_'.$faq->id }}">
+                                <h2 class="accordion-header" id="heading_{{$loop->iteration}}">
+
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_{{$loop->iteration}}" aria-expanded="false" aria-controls="collapse_{{$loop->iteration}}">
+
+
                                         {{ $faq->question }}
                                     </button>
+
                                 </h2>
-                                <div id="{{ 'collapse_'.$faq->id }}" class="accordion-collapse collapse" aria-labelledby="{{ 'heading_'.$faq->id }}" data-bs-parent="#accordionExample">
+
+
+                                <div id="collapse_{{$loop->iteration}}" class="accordion-collapse collapse" aria-labelledby="heading_{{$loop->iteration}}" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
+
                                         {!! $faq->answer !!}
+
                                     </div>
                                 </div>
+
+
                             </div>
                         @endforeach
                     </div>
