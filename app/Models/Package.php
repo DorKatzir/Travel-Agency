3800d4096223e68dcee7bfd0e48598a6a9c3f06a
Dor Katzir <katzirdor@gmail.com>
2024-08-30 19:37:36 +0300

    Section 19 - Package Itinerary - Part 1

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -7,9 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Package extends Model
 {
+
     use HasFactory;
 
     public function destination() {
         return $this->belongsTo(Destination::class);
     }
+
+    public function package_itineraries() {
+        return $this->hasMany(PackageItinerary::class);
+    }
+
 }
