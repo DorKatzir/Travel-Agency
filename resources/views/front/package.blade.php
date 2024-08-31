3736b62288e5374362d40a4cfd41b45d69af043d
Dor Katzir <katzirdor@gmail.com>
2024-08-31 23:29:14 +0300

    Section 23 - Package - Video Gallery - Part 02

--- resources/views/front/package.blade.php
+++ resources/views/front/package.blade.php
@@ -51,9 +51,12 @@
                             <li class="nav-item" role="presentation">
                                 <button class="nav-link" id="tab-3" data-bs-toggle="tab" data-bs-target="#tab-3-pane" type="button" role="tab" aria-controls="tab-3-pane" aria-selected="false">Location</button>
                             </li>
-                            <li class="nav-item" role="presentation">
-                                <button class="nav-link" id="tab-4" data-bs-toggle="tab" data-bs-target="#tab-4-pane" type="button" role="tab" aria-controls="tab-4-pane" aria-selected="false">Gallery</button>
-                            </li>
+                            @if ( $package_photos->count() > 0 || $package_videos->count() > 0 )
+                                <li class="nav-item" role="presentation">
+                                    <button class="nav-link" id="tab-4" data-bs-toggle="tab" data-bs-target="#tab-4-pane" type="button" role="tab" aria-controls="tab-4-pane" aria-selected="false">Gallery</button>
+                                </li>
+                            @endif
+
                             <li class="nav-item" role="presentation">
                                 <button class="nav-link" id="tab-5" data-bs-toggle="tab" data-bs-target="#tab-5-pane" type="button" role="tab" aria-controls="tab-5-pane" aria-selected="false">FAQ</button>
                             </li>
@@ -133,44 +136,48 @@
 
                             <div class="tab-pane fade" id="tab-4-pane" role="tabpanel" aria-labelledby="tab-4" tabindex="0">
                                 <!-- Gallery -->
-                                <h2 class="mt_30">
-                                    Photos
-                                </h2>
-                                <div class="photo-all">
-                                    <div class="row">
-                                        @foreach ($package_photos as $package_photo)
-                                            <div class="col-md-6 col-lg-3">
+                                @if ( $package_photos->count() > 0 )
+                                    <h2 class="mt_30">
+                                        Photos
+                                    </h2>
+                                    <div class="photo-all">
+                                        <div class="row">
+                                            @foreach ($package_photos as $package_photo)
+                                                <div class="col-md-6 col-lg-3">
+                                                    <div class="item">
+                                                        <a href="{{ asset('uploads/'.$package_photo->photo) }}" class="magnific">
+                                                            <img src="{{ asset('uploads/'.$package_photo->photo) }}" alt="">
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                @endif
+
+                                @if ( $package_videos->count() > 0 )
+                                    <h2 class="mt_30">
+                                        Videos
+                                    </h2>
+                                    <div class="video-all">
+                                        <div class="row">
+                                            @foreach ($package_videos as $package_video)
+                                            <div class="col-md-6 col-lg-6">
                                                 <div class="item">
-                                                    <a href="{{ asset('uploads/'.$package_photo->photo) }}" class="magnific">
-                                                        <img src="{{ asset('uploads/'.$package_photo->photo) }}" alt="">
+                                                    <a class="video-button" href="http://www.youtube.com/watch?v={{ $package_video->video }}">
+                                                        <img src="http://img.youtube.com/vi/{{ $package_video->video }}/0.jpg" alt="">
+                                                        <div class="icon">
+                                                            <i class="far fa-play-circle"></i>
+                                                        </div>
+                                                        <div class="bg"></div>
                                                     </a>
                                                 </div>
                                             </div>
                                         @endforeach
-                                    </div>
-                                </div>
-
-
-                                <h2 class="mt_30">
-                                    Videos
-                                </h2>
-                                <div class="video-all">
-                                    <div class="row">
-                                        @foreach ($package_videos as $package_video)
-                                        <div class="col-md-6 col-lg-6">
-                                            <div class="item">
-                                                <a class="video-button" href="http://www.youtube.com/watch?v={{ $package_video->video }}">
-                                                    <img src="http://img.youtube.com/vi/{{ $package_video->video }}/0.jpg" alt="">
-                                                    <div class="icon">
-                                                        <i class="far fa-play-circle"></i>
-                                                    </div>
-                                                    <div class="bg"></div>
-                                                </a>
-                                            </div>
                                         </div>
-                                       @endforeach
                                     </div>
-                                </div>
+                                @endif
+
                                 <!-- // Gallery -->
                             </div>
 
