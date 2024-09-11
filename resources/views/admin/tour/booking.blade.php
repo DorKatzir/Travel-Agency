d715cb5aebd1fb92c31f4fa89d0a4b7598cb8637
Dor Katzir <katzirdor@gmail.com>
2024-09-11 22:42:02 +0300

    Section 26 - Package Booking - Part 8

--- resources/views/admin/tour/booking.blade.php
+++ resources/views/admin/tour/booking.blade.php
@@ -18,55 +18,74 @@
                     </a>
                 </div>
             </div>
+
+
             <div class="section-body">
                 <div class="row">
                     <div class="col-12">
-                        {{--  --}}
                         <div class="card">
                             <div class="card-body">
+
                                 <div class="table-responsive">
                                     <table class="table table-bordered" id="example1">
                                         <thead>
                                             <tr>
                                                 <th>SL</th>
                                                 <th>Invoice No</th>
-                                                {{-- <th>Package Name</th> --}}
                                                 <th>User Info</th>
-                                                <th class="w_50">Persons</th>
+                                                <th class="w_50">Total Persons</th>
                                                 <th>Paid Amount</th>
-                                                <th>Payment</th>
-                                                <th>Status</th>
-
+                                                <th>Payment Method</th>
+                                                <th>Payment Status</th>
+                                                <th>Action</th>
                                             </tr>
                                         </thead>
-
                                         <tbody>
-                                            @foreach ($booking_data as $booking)
-                                            <tr>
-                                                <td>{{ $loop->iteration }}</td>
-                                                <td>{{ $booking->invoice_no }}</td>
-                                                {{-- <td>{{ $booking->package->name }}</td> --}}
-                                                <td>
-                                                    <strong>Name: </strong> {{ $booking->user->name }} <br>
-                                                    <strong>Email: </strong> {{ $booking->user->email }} <br>
-                                                    <strong>Phone: </strong> {{ $booking->user->phone }}
-                                                </td>
 
-                                                <td>{{ $booking->total_person }}</td>
-                                                <td>{{ $booking->paid_amount }}</td>
-                                                <td>{{ $booking->payment_method }}</td>
-                                                <td>{{ $booking->payment_status }}</td>
-                                            </tr>
+                                            @foreach ($booking_data as $booking)
+
+                                                <tr>
+                                                    <td>{{ $loop->iteration }}</td>
+                                                    <td>{{ $booking->invoice_no }}</td>
+                                                    <td>
+                                                        <strong>Name: </strong>{{ $booking->user->name }}<br>
+                                                        <strong>Email: </strong>{{ $booking->user->email }}<br>
+                                                        <strong>Phone: </strong>{{ $booking->user->phone }}<br>
+                                                    </td>
+                                                    <td>{{ $booking->total_person }}</td>
+                                                    <td>{{ $booking->paid_amount }}</td>
+                                                    <td>{{ $booking->payment_method }}</td>
+
+                                                    @if ($booking->payment_status == 'Completed')
+                                                        <td><span class="badge badge-success">Completed</span></td>
+                                                    @else
+                                                        <td><span class="badge badge-danger">Pending</span></td>
+                                                    @endif
+
+
+
+
+
+                                                    <td class="pt_10 pb_10">
+
+                                                        <a href="#" class="btn btn-danger" onClick="return confirm('Are you sure?');"><i class="fas fa-trash"></i></a>
+                                                    </td>
+                                                </tr>
+
                                             @endforeach
+
                                         </tbody>
                                     </table>
                                 </div>
+
+
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
+
+
         </section>
 
     </div>
