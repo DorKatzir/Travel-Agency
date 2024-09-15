41b36a3137149540e3f553ff1940027f9cecc6b2
Dor Katzir <katzirdor@gmail.com>
2024-09-15 23:04:34 +0300

    Section 26 - Package Booking Part 11

--- resources/views/user/invoice.blade.php
+++ resources/views/user/invoice.blade.php
@@ -1,1 +1,24 @@
-{{ $booking->invoice_no }}
+@extends('front.layout.master')
+@section('main_content')
+
+    <div class="page-top" style="background-image: url({{ asset('uploads/banner.jpg') }})">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Invoice</h2>
+                    <div class="breadcrumb-container">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                            <li class="breadcrumb-item active">Invoice</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+@endsection
+
