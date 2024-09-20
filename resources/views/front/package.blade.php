5ebcf78f7fda0b085f4cc4a5957ba511bcf62d31
Dor Katzir <katzirdor@gmail.com>
2024-09-20 16:51:22 +0300

    Section 27 - Package Review - 1 / 4

--- resources/views/front/package.blade.php
+++ resources/views/front/package.blade.php
@@ -254,44 +254,56 @@
                                     <h2>Leave Your Review</h2>
                                     @if (Auth::guard('web')->check())
 
+                                        @php
+                                            $allowed_review = App\Models\Booking::where('user_id', Auth::guard('web')->user()->id)->where('package_id', $package->id)->where('payment_status', 'Completed')->count();
+                                        @endphp
 
-                                        <div class="mb-3">
-                                            <div class="give-review-auto-select">
-                                                <input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="5 stars"><i class="fas fa-star"></i></label>
-                                                <input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="4 stars"><i class="fas fa-star"></i></label>
-                                                <input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="3 stars"><i class="fas fa-star"></i></label>
-                                                <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="2 stars"><i class="fas fa-star"></i></label>
-                                                <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="1 star"><i class="fas fa-star"></i></label>
-                                            </div>
-                                            <script>
-                                                document.addEventListener('DOMContentLoaded', (event) => {
-                                                    const stars = document.querySelectorAll('.star-rating label');
-                                                    stars.forEach(star => {
-                                                        star.addEventListener('click', function() {
-                                                            stars.forEach(s => s.style.color = '#ccc');
-                                                            this.style.color = '#f5b301';
-                                                            let previousStar = this.previousElementSibling;
-                                                            while(previousStar) {
-                                                                if (previousStar.tagName === 'LABEL') {
-                                                                    previousStar.style.color = '#f5b301';
+                                        @if ($allowed_review > 0)
+
+                                            <div class="mb-3">
+                                                <div class="give-review-auto-select">
+                                                    <input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="5 stars"><i class="fas fa-star"></i></label>
+                                                    <input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="4 stars"><i class="fas fa-star"></i></label>
+                                                    <input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="3 stars"><i class="fas fa-star"></i></label>
+                                                    <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="2 stars"><i class="fas fa-star"></i></label>
+                                                    <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="1 star"><i class="fas fa-star"></i></label>
+                                                </div>
+                                                <script>
+                                                    document.addEventListener('DOMContentLoaded', (event) => {
+                                                        const stars = document.querySelectorAll('.star-rating label');
+                                                        stars.forEach(star => {
+                                                            star.addEventListener('click', function() {
+                                                                stars.forEach(s => s.style.color = '#ccc');
+                                                                this.style.color = '#f5b301';
+                                                                let previousStar = this.previousElementSibling;
+                                                                while(previousStar) {
+                                                                    if (previousStar.tagName === 'LABEL') {
+                                                                        previousStar.style.color = '#f5b301';
+                                                                    }
+                                                                    previousStar = previousStar.previousElementSibling;
                                                                 }
-                                                                previousStar = previousStar.previousElementSibling;
-                                                            }
+                                                            });
                                                         });
                                                     });
-                                                });
-                                            </script>
-                                        </div>
-                                        <div class="mb-3">
-                                            <textarea class="form-control" rows="3" placeholder="Comment"></textarea>
-                                        </div>
-                                        <div class="mb-3">
-                                            <button type="submit" class="btn btn-primary">Submit</button>
-                                        </div>
+                                                </script>
+                                            </div>
+                                            <div class="mb-3">
+                                                <textarea class="form-control" rows="3" placeholder="Comment"></textarea>
+                                            </div>
+                                            <div class="mb-3">
+                                                <button type="submit" class="btn btn-primary">Submit</button>
+                                            </div>
+                                        @else
+                                            <div class="mb-3">
+                                                <p>You have to book this package to give review.</p>
+                                            </div>
+                                        @endif
+
                                     @else
                                         <div class="mb-3">
                                             <a href="{{ route('login') }}" class="btn btn-danger">Login to Review</a>
                                         </div>
+
                                     @endif
                                 </div>
                                 <!-- // Review -->
