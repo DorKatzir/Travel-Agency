298a159568dee20652b5ce3ab9979e176fe01765
Dor Katzir <katzirdor@gmail.com>
2024-09-23 01:01:44 +0300

    Section 28 - Package - Listing or Search Page - 1/4

--- resources/views/front/packages.blade.php
+++ resources/views/front/packages.blade.php
@@ -2,14 +2,14 @@
 
 @section('main_content')
 
-    <div class="page-top" style="background-image: url('uploads/banner.jpg')">
+    <div class="page-top" style="background-image: url({{ asset('uploads/banner.jpg') }})">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <h2>Packages</h2>
                     <div class="breadcrumb-container">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
                             <li class="breadcrumb-item active">Packages</li>
                         </ol>
                     </div>
