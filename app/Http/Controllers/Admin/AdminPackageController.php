a86f81c4a315e152ce0a3ccd59cf4b2eee229bce
Dor Katzir <katzirdor@gmail.com>
2024-09-22 14:32:19 +0300

    Section 27 - Package Review - 3/4

--- app/Http/Controllers/Admin/AdminPackageController.php
+++ app/Http/Controllers/Admin/AdminPackageController.php
@@ -55,6 +55,8 @@ class AdminPackageController extends Controller
         $obj->map = $request->map;
         $obj->price = $request->price;
         $obj->old_price = $request->old_price;
+        $obj->total_rating = 0;
+        $obj->total_score = 0;
 
         $obj->save();
 
--- app/Http/Controllers/Admin/AdminReviewController.php
+++ app/Http/Controllers/Admin/AdminReviewController.php
@@ -21,7 +21,7 @@ class AdminReviewController extends Controller
         $review->delete();
 
         $package = Package::where('id', $review_package_id)->first();
-        $package->total_rating =- 1;
+        $package->total_rating -= 1;
         $package->total_score -= $review_rating;
         $package->update();
 
