03e649605286f9f85eb4bc828303468ea7886509
Dor Katzir <katzirdor@gmail.com>
2024-09-22 22:27:40 +0300

    Section 27 - Package Review - 4/4

--- resources/views/user/review.blade.php
+++ resources/views/user/review.blade.php
@@ -1,14 +1,14 @@
 @extends('front.layout.master')
 @section('main_content')
 
-<div class="page-top" style="background-image: url('uploads/banner.jpg')">
+<div class="page-top" style="background-image: url({{ asset('uploads/banner.jpg') }})">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <h2>Reviews</h2>
                 <div class="breadcrumb-container">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
                         <li class="breadcrumb-item active">Reviews</li>
                     </ol>
                 </div>
@@ -38,79 +38,58 @@
                                 <th>My Comment</th>
                                 <th class="w-100">Action</th>
                             </tr>
-                            <tr>
-                                <td>1</td>
-                                <td>
-                                    <img src="uploads/package-1.jpg" alt="" class="w-200">
-                                </td>
-                                <td>
-                                    Royal Ontario Museum
-                                </td>
-                                <td>
-                                    Canada
-                                </td>
-                                <td>
-                                    <div class="review">
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="far fa-star"></i>
-                                    </div>
-                                </td>
-                                <td>
-                                    <a href="" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">Comment</a>
-                                </td>
-                                <td>
-                                    <a href="package.html" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>
-                                    <img src="uploads/package-2.jpg" alt="" class="w-200">
-                                </td>
-                                <td>
-                                    Great Barrier Reef
-                                </td>
-                                <td>
-                                    Australia
-                                </td>
-                                <td>
-                                    <div class="review">
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="far fa-star"></i>
-                                    </div>
-                                </td>
-                                <td>
-                                    <a href="" class="btn btn-primary btn-sm">Comment</a>
-                                </td>
-                                <td>
-                                    <a href="package.html" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
-                                </td>
-                            </tr>
-                            <!-- Modal -->
-                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Comment</h1>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            @foreach ($reviews as $review)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>
+                                        <img src="{{ asset('uploads/'.$review->package->featured_photo) }}" alt="" class="w-100 rounded">
+                                    </td>
+                                    <td>
+                                        {{ $review->package->name }}
+                                    </td>
+                                    <td>
+                                        {{ $review->package->destination->name }}
+                                    </td>
+                                    <td>
+                                        <div class="review">
+                                            @for ($i = 1; $i <= 5; $i++)
+                                                @if ($i <= $review->rating)
+                                                    <i class="fas fa-star"></i>
+                                                @else
+                                                    <i class="far fa-star"></i>
+                                                @endif
+                                            @endfor
+
                                         </div>
-                                        <div class="modal-body">
-                                            <div class="mb-3 row">
-                                                <div class="col-md-12">
-                                                    The tour was amazing. I enjoyed it a lot. If you want to visit Canada, I will recommend this tour.
+                                    </td>
+                                    <td>
+                                        <a href="" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal{{ $loop->iteration }}">Comment</a>
+                                    </td>
+                                    <td>
+                                        <a href="package.html" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
+                                    </td>
+                                </tr>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModal{{ $loop->iteration }}" tabindex="-1" aria-labelledby="exampleModalLabel{{ $loop->iteration }}" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel{{ $loop->iteration }}">Comment</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="mb-3 row">
+                                                    <div class="col-md-12">
+                                                        {!! $review->comment !!}
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <!-- // Modal -->
+                                <!-- // Modal -->
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
