8f9031089f42d07d28d92135151848b6187ac3cb
Dor Katzir <katzirdor@gmail.com>
2024-09-23 23:48:37 +0300

    Section 28 - Package - Listing or Search Page - 1/4

--- resources/views/front/packages.blade.php
+++ resources/views/front/packages.blade.php
@@ -23,95 +23,99 @@
             <div class="row">
                 <div class="col-lg-4 col-md-6">
                     <div class="package-sidebar">
-                        <div class="widget">
-                            <h2>Search Package</h2>
-                            <div class="box">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <input type="text" name="" class="form-control" placeholder="Package Name ...">
+                        <form action="" method="GET">
+                            @csrf
+                            <div class="widget">
+                                <h2>Search Package</h2>
+                                <div class="box">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <input type="text" name="" class="form-control" placeholder="Package Name ...">
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="widget">
-                            <h2>Filter by Price</h2>
-                            <div class="box">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <input type="text" name="" class="form-control" placeholder="Min">
+                            <div class="widget">
+                                <h2>Filter by Price</h2>
+                                <div class="box">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <input type="text" name="" class="form-control" placeholder="Min">
+                                        </div>
+                                        <div class="col-md-6">
+                                            <input type="text" name="" class="form-control" placeholder="Max">
+                                        </div>
                                     </div>
-                                    <div class="col-md-6">
-                                        <input type="text" name="" class="form-control" placeholder="Max">
+                                </div>
+                            </div>
+                            <div class="widget">
+                                <h2>Filter by Destination</h2>
+                                <div class="box">
+                                    <select class="form-control" name="destination_id">
+                                        <option value="">Select Destination</option>
+                                        @foreach ($packages as $package)
+                                            <option value="{{ $package->destination->id }}">{{ $package->destination->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="widget">
+                                <h2>Filter by Review</h2>
+                                <div class="box">
+                                    <div class="form-check form-check-review form-check-review-1">
+                                        <input name="review" class="form-check-input" type="radio" name="reviewRadios" id="reviewRadiosAll" value="all" checked>
+                                        <label class="form-check-label" for="reviewRadiosAll">
+                                            All
+                                        </label>
+                                    </div>
+                                    <div class="form-check form-check-review">
+                                        <input name="review" class="form-check-input" type="radio" name="reviewRadios" id="reviewRadios1" value="5" checked>
+                                        <label class="form-check-label" for="reviewRadios1">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                        </label>
+                                    </div>
+                                    <div class="form-check form-check-review">
+                                        <input name="review" class="form-check-input" type="radio" name="reviewRadios" id="reviewRadios2" value="4">
+                                        <label class="form-check-label" for="reviewRadios2">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                        </label>
+                                    </div>
+                                    <div class="form-check form-check-review">
+                                        <input name="review" class="form-check-input" type="radio" name="reviewRadios" id="reviewRadios3" value="3">
+                                        <label class="form-check-label" for="reviewRadios3">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                        </label>
+                                    </div>
+                                    <div class="form-check form-check-review">
+                                        <input name="review" class="form-check-input" type="radio" name="reviewRadios" id="reviewRadios4" value="2">
+                                        <label class="form-check-label" for="reviewRadios4">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                        </label>
+                                    </div>
+                                    <div class="form-check form-check-review">
+                                        <input name="review" class="form-check-input" type="radio" name="reviewRadios" id="reviewRadios5" value="1">
+                                        <label class="form-check-label" for="reviewRadios5">
+                                            <i class="fas fa-star"></i>
+                                        </label>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="widget">
-                            <h2>Filter by Destination</h2>
-                            <div class="box">
-                                <select class="form-control" name="destination_id">
-                                    <option value="">Select Destination</option>
-                                    @foreach ($packages as $package)
-                                        <option value="{{ $package->destination->id }}">{{ $package->destination->name }}</option>
-                                    @endforeach
-                                </select>
+                            <div class="filter-button">
+                                <button class="btn btn-primary">Filter</button>
                             </div>
-                        </div>
-                        <div class="widget">
-                            <h2>Filter by Review</h2>
-                            <div class="box">
-                                <div class="form-check form-check-review form-check-review-1">
-                                    <input name="review" class="form-check-input" type="radio" name="reviewRadios" id="reviewRadiosAll" value="option1" checked>
-                                    <label class="form-check-label" for="reviewRadiosAll">
-                                        All
-                                    </label>
-                                </div>
-                                <div class="form-check form-check-review">
-                                    <input name="review" class="form-check-input" type="radio" name="reviewRadios" id="reviewRadios1" value="option1" checked>
-                                    <label class="form-check-label" for="reviewRadios1">
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                    </label>
-                                </div>
-                                <div class="form-check form-check-review">
-                                    <input name="review" class="form-check-input" type="radio" name="reviewRadios" id="reviewRadios2" value="option2">
-                                    <label class="form-check-label" for="reviewRadios2">
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                    </label>
-                                </div>
-                                <div class="form-check form-check-review">
-                                    <input name="review" class="form-check-input" type="radio" name="reviewRadios" id="reviewRadios3" value="option2">
-                                    <label class="form-check-label" for="reviewRadios3">
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                    </label>
-                                </div>
-                                <div class="form-check form-check-review">
-                                    <input name="review" class="form-check-input" type="radio" name="reviewRadios" id="reviewRadios4" value="option2">
-                                    <label class="form-check-label" for="reviewRadios4">
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                    </label>
-                                </div>
-                                <div class="form-check form-check-review">
-                                    <input name="review" class="form-check-input" type="radio" name="reviewRadios" id="reviewRadios5" value="option2">
-                                    <label class="form-check-label" for="reviewRadios5">
-                                        <i class="fas fa-star"></i>
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="filter-button">
-                            <button class="btn btn-primary">Filter</button>
-                        </div>
+                    </form>
                     </div>
+
                 </div>
                 <div class="col-lg-8 col-md-6">
                     {{-- Packages --}}
