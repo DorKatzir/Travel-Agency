4e6ba67db349278821ffad35b930e2be88cac808
Dor Katzir <katzirdor@gmail.com>
2024-09-25 22:47:29 +0300

    Section 28 - Package - Listing or Search Page - 3/4

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -111,11 +111,11 @@ class FrontController extends Controller
         $form_name = $request->name;
         $form_min_price = $request->min_price;
         $form_max_price = $request->max_price;
-        $form_destination = $request->destination_id;
+        $form_destination_id = $request->destination_id;
         $form_review = $request->review;
 
         $destinations = Destination::orderBy('name', 'asc')->get();
-        $packages = Package::orderBy('id', 'desc')->paginate(4);
+        $packages = Package::orderBy('id', 'desc')->paginate(1);
 
         return view('front.packages', compact('packages', 'destinations'));
 
