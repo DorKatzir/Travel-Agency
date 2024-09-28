4415031e91d01f4965429cae74aee7826931a05c
Dor Katzir <katzirdor@gmail.com>
2024-09-28 21:20:56 +0300

    Section 30 - User  Wishlist  - 01/02

--- database/migrations/2024_09_28_181151_create_wishlists_table.php
+++ database/migrations/2024_09_28_181151_create_wishlists_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('wishlists', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('package_id');
             $table->timestamps();
         });
     }
