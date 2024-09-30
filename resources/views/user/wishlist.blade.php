29fa8a78af8aa434f7598755adbb1940eceb6537
Dor Katzir <katzirdor@gmail.com>
2024-09-30 23:02:27 +0300

    Section 30 - User  Wishlist  - 02/02

--- resources/views/user/wishlist.blade.php
+++ resources/views/user/wishlist.blade.php
@@ -26,37 +26,43 @@
                     </div>
                 </div>
                 <div class="col-lg-9 col-md-12">
-                    <div class="table-responsive">
-                        <table class="table table-bordered">
-                            <tbody>
-                                <tr>
-                                    <th>SL</th>
-                                    <th>Photo</th>
-                                    <th>Package</th>
-                                    <th>Destination</th>
-                                    <th class="w-100">Action</th>
-                                </tr>
-                                @foreach ($wishlist as $item)
+                    @if ($wishlist->count() > 0 )
+                        <div class="table-responsive">
+                            <table class="table table-bordered">
+                                <tbody>
                                     <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>
-                                            <img src="{{ asset('uploads/' . $item->package->featured_photo) }}" alt="" class="w-150 rounded">
-                                        </td>
-                                        <td>
-                                            {{ $item->package->name }}
-                                        </td>
-                                        <td>
-                                            {{ $item->package->destination->country }}
-                                        </td>
-                                        <td>
-                                            <a href="{{ route('package', $item->package->slug) }}" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
-                                            <a href="" class="btn btn-danger btn-sm" onClick="return confirm('Are You Sure?')"><i class="fas fa-trash"></i></a>
-                                        </td>
+                                        <th>SL</th>
+                                        <th>Photo</th>
+                                        <th>Package</th>
+                                        <th>Destination</th>
+                                        <th class="w-100">Action</th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                                    @foreach ($wishlist as $item)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>
+                                                <img src="{{ asset('uploads/' . $item->package->featured_photo) }}" alt="" class="w-150 rounded">
+                                            </td>
+                                            <td>
+                                                {{ $item->package->name }}
+                                            </td>
+                                            <td>
+                                                {{ $item->package->destination->country }}
+                                            </td>
+                                            <td>
+                                                <a href="{{ route('package', $item->package->slug) }}" class="btn btn-primary btn-sm" target="_blank"><i class="fas fa-eye"></i></a>
+                                                <a href="" class="btn btn-danger btn-sm" onClick="return confirm('Are You Sure?')"><i class="fas fa-trash"></i></a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    @else
+                        <div class="alert alert-info">
+                            Your Wishlist is empty.
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
