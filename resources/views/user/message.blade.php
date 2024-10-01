c623d39613b8296fd835a2a95746d002d83e1b5e
Dor Katzir <katzirdor@gmail.com>
2024-10-02 00:14:03 +0300

    Section 31 - Message Between User & Admin -  1 / 3

--- resources/views/user/message.blade.php
+++ resources/views/user/message.blade.php
@@ -1,14 +1,14 @@
 @extends('front.layout.master')
 @section('main_content')
 
-    <div class="page-top" style="background-image: url('uploads/banner.jpg')">
+    <div class="page-top" style="background-image: url('{{ asset('uploads/banner.jpg') }}')">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <h2>Message</h2>
                 <div class="breadcrumb-container">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
                         <li class="breadcrumb-item active">Message</li>
                     </ol>
                 </div>
