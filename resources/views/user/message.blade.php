e69283c3008d3b995df0a994db16791420c8ae1b
Dor Katzir <katzirdor@gmail.com>
2024-10-02 00:11:15 +0300

    Section 31 - Message Between User & Admin -  1 / 3

--- resources/views/user/message.blade.php
+++ resources/views/user/message.blade.php
@@ -1,6 +1,81 @@
 @extends('front.layout.master')
 @section('main_content')
 
-    Message
+    <div class="page-top" style="background-image: url('uploads/banner.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Message</h2>
+                <div class="breadcrumb-container">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item active">Message</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="page-content user-panel pt_70 pb_70">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3 col-md-12">
+                <div class="card">
+                   @include('user.sidebar')
+                </div>
+            </div>
+            <div class="col-lg-5 col-md-12">
+                <h3>All Messages</h3>
+                <div class="message-item message-item-admin-border">
+                    <div class="message-top">
+                        <div class="left">
+                            <img src="uploads/user-photo.jpg" alt="">
+                        </div>
+                        <div class="right">
+                            <h4>Morshedul Arefin</h4>
+                            <h5>Admin</h5>
+                            <div class="date-time">2024-08-20 09:33:22 AM</div>
+                        </div>
+                    </div>
+                    <div class="message-bottom">
+                        <p>Thank you for contacting. Sure, you can take it with you without any problem.</p>
+                    </div>
+                </div>
+
+                <div class="message-item">
+                    <div class="message-top">
+                        <div class="left">
+                            <img src="uploads/team-1.jpg" alt="">
+                        </div>
+                        <div class="right">
+                            <h4>Smith Brent</h4>
+                            <h5>Client</h5>
+                            <div class="date-time">2024-08-20 08:12:43 AM</div>
+                        </div>
+                    </div>
+                    <div class="message-bottom">
+                        <p>I forgot to tell one thing. Can you please allow some toys for my son in this tour? It will be very much helpful if you allow.</p>
+                    </div>
+                </div>
+
+
+
+            </div>
+
+            <div class="col-lg-4 col-md-12">
+                <h3>Write a message</h3>
+                <form action="" method="post">
+                    <div class="mb-2">
+                        <textarea name="" class="form-control h-150" cols="30" rows="10" placeholder="Write your message here"></textarea>
+                    </div>
+                    <div class="mb-2">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    </div>
 
 @endsection
