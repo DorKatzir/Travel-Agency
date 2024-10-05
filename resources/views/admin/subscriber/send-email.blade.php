8d9d5160c51266564a09c0df5a1017eda452df04
Dor Katzir <katzirdor@gmail.com>
2024-10-05 22:34:32 +0300

    Section 32 - Subscriber -  2 / 2

--- resources/views/admin/subscriber/send-email.blade.php
+++ resources/views/admin/subscriber/send-email.blade.php
@@ -10,59 +10,44 @@
 
         <section class="section">
             <div class="section-header justify-content-between">
-                <h1>Send Email to Subscribers</h1>
+                <h1>Send Email to All</h1>
                 <div class="ml-auto">
                     <a href="{{ route('admin_subscribers') }}" class="btn btn-primary">
                         Back to Subscribers
                     </a>
                 </div>
             </div>
-            {{-- <div class="section-body">
+
+            <div class="section-body">
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
                             <div class="card-body">
 
-                                <div class="table-responsive">
-                                    <table class="table table-bordered" id="example1">
-                                        <thead>
-                                            <tr>
-                                                <th>SL</th>
-                                                <th>Email</th>
-                                                <th>Status</th>
-                                                <th>Action</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
+                                <form method="POST" action="">
+                                    @csrf
+                                        <div class="mb-3">
+                                            <label class="form-label">Subject *</label>
+                                            <input type="text" class="form-control" name="subject">
+                                        </div>
 
-                                            @foreach ($subscribers as $subscriber)
+                                        <div class="mb-3">
+                                            <label class="form-label">Message *</label>
+                                            <textarea name="message" class="form-control h_150" cols="30" rows="10"></textarea>
+                                        </div>
 
-                                                <tr>
-                                                    <td>{{ $loop->iteration }}</td>
+                                        <div class="mb-3">
+                                            <button type="submit" class="btn btn-primary">Send Email</button>
+                                        </div>
 
-                                                    <td>
-                                                        {{ $subscriber->email }}
-                                                    </td>
-
-                                                    <td>{{ $subscriber->status }}</td>
-
-                                                    <td class="pt_10 pb_10">
-                                                        <a href="{{ route('admin_subscriber_delete', $subscriber->id) }}" class="btn btn-danger" onClick="return confirm('Are you sure?');"><i class="fas fa-trash"></i></a>
-                                                    </td>
-                                                </tr>
-
-                                            @endforeach
-
-                                        </tbody>
-                                    </table>
-                                </div>
+                                </form>
 
 
                             </div>
                         </div>
                     </div>
                 </div>
-            </div> --}}
+            </div>
         </section>
 
     </div>
