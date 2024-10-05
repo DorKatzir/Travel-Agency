544ba1288486bbc091b8d8e339b2f15d99baacd4
Dor Katzir <katzirdor@gmail.com>
2024-10-05 22:44:34 +0300

    Section 32 - Subscriber -  2 / 2

--- app/Http/Controllers/Admin/AdminSubscriberController.php
+++ app/Http/Controllers/Admin/AdminSubscriberController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Mail\Websitemail;
 use App\Models\Subscriber;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -17,6 +18,23 @@ class AdminSubscriberController extends Controller
     public function subscribers_send_email() {
 
         return view('admin.subscriber.send-email');
+    }
+
+    public function subscribers_send_email_submit(Request $request) {
+        $request->validate([
+            'subject' => 'required',
+            'message' => 'required',
+        ]);
+        $subject = $request->subject;
+        $message = $request->message;
+
+        $subscribers = Subscriber::where('status', 'Active')->get();
+        foreach($subscribers as $subscriber) {
+            \Mail::to($subscriber->email)->send(new Websitemail($subject,$message));
+        }
+
+        return redirect()->back()->with('success', 'Email has been sent to all subscribers');
+
     }
 
     public function subscriber_delete($id) {
