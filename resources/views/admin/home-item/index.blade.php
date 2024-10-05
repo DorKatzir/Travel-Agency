484bf1ec389e147a37bc8f77df47be7a8f6dd941
Dor Katzir <katzirdor@gmail.com>
2024-10-06 01:56:54 +0300

    Section 33 - Homepage  -  1 / 2

--- resources/views/admin/home-item/index.blade.php
+++ resources/views/admin/home-item/index.blade.php
@@ -21,7 +21,7 @@
 
                                 <form method="POST" action="{{ route('admin_homeItem_update', $homeItem->id) }}">
                                     @csrf
-
+                                    {{-- Destination --}}
                                     <div class="mb-3">
                                         <label class="form-label">Destination Heading *</label>
                                         <input type="text" class="form-control" name="destination_heading" value="{{ $homeItem->destination_heading }}">
@@ -33,80 +33,15 @@
                                     </div>
 
                                     <div class="mb-3">
-                                        <label class="form-label">Status </label>
-                                        <select name="status" class="form-select">
+                                        <label class="form-label">Destination Status </label>
+                                        <select name="destination_status" class="form-select">
                                             <option value="show" {{ $homeItem->destination_status == 'show' ? 'selected' : ''  }}>show</option>
                                             <option value="hide" {{ $homeItem->destination_status == 'hide' ? 'selected' : ''  }}>hide</option>
                                         </select>
                                     </div>
+                                    {{-- End Destination --}}
 
 
-
-                                    <div class="row mb-4">
-                                        <div class="col-md-4">
-
-                                        </div>
-                                        <div class="col-md-4">
-                                            <div class="mb-3">
-                                                <label class="form-label">Item 1 - Text *</label>
-                                                <input type="text" class="form-control" name="item1_text" value="{{ $homeItem->item1_text }}">
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="row mb-4">
-                                        <div class="col-md-4">
-                                            <div class="mb-3">
-                                                <label class="form-label">Item 2 - Number *</label>
-                                                <input type="text" class="form-control" name="item2_number" value="{{ $homeItem->item2_number }}">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <div class="mb-3">
-                                                <label class="form-label">Item 2 - Text *</label>
-                                                <input type="text" class="form-control" name="item2_text" value="{{ $homeItem->item2_text }}">
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="row mb-4">
-                                        <div class="col-md-4">
-                                            <div class="mb-3">
-                                                <label class="form-label">Item 3 - Number *</label>
-                                                <input type="text" class="form-control" name="item3_number" value="{{ $homeItem->item3_number }}">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <div class="mb-3">
-                                                <label class="form-label">Item 3 - Text *</label>
-                                                <input type="text" class="form-control" name="item3_text" value="{{ $homeItem->item3_text }}">
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="row mb-4">
-                                        <div class="col-md-4">
-                                            <div class="mb-3">
-                                                <label class="form-label">Item 4 - Number *</label>
-                                                <input type="text" class="form-control" name="item4_number" value="{{ $homeItem->item4_number }}">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <div class="mb-3">
-                                                <label class="form-label">Item 4 - Text *</label>
-                                                <input type="text" class="form-control" name="item4_text" value="{{ $homeItem->item4_text }}">
-                                            </div>
-                                        </div>
-                                    </div>
-
-
-                                    <div class="mb-3">
-                                        <label class="form-label">Status </label>
-                                        <select name="status" class="form-select">
-                                            <option value="show" {{ $homeItem->status == 'show' ? 'selected' : ''  }}>show</option>
-                                            <option value="hide" {{ $homeItem->status == 'hide' ? 'selected' : ''  }}>hide</option>
-                                        </select>
-                                    </div>
 
                                     <div class="mb-3">
                                         <button type="submit" class="btn btn-primary">Update</button>
