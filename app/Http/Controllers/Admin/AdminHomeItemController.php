f9bbb5b2d2c6daac8b0552ccd5fcefb0396b4c58
Dor Katzir <katzirdor@gmail.com>
2024-10-06 02:22:38 +0300

    Section 33 - Homepage  -  1 / 2

--- app/Http/Controllers/Admin/AdminHomeItemController.php
+++ app/Http/Controllers/Admin/AdminHomeItemController.php
@@ -15,19 +15,31 @@ class AdminHomeItemController extends Controller
 
     public function homeItem_update(Request $request) {
 
-        dd($request->all());
+        // dd($request->all());
 
-        // $request->validate([
-        //     'destination_heading' => 'required',
-        //     'destination_subheading' => 'required',
-        //     'destination_status' => 'required',
+        $request->validate([
+            'destination_heading' => 'required',
+            'destination_subheading' => 'required',
+            'package_heading' => 'required',
+            'package_subheading' => 'required',
+            'testimonial_heading' => 'required',
+            'testimonial_subheading' => 'required',
+            'blog_heading' => 'required',
+            'blog_subheading' => 'required',
+        ]);
 
-        // ]);
+        $obj = HomeItem::where('id', 1)->first();
+        $obj->destination_heading = $request->destination_heading;
+        $obj->destination_subheading = $request->destination_subheading;
+        $obj->package_heading = $request->package_heading;
+        $obj->package_subheading = $request->package_subheading;
+        $obj->testimonial_heading = $request->testimonial_heading;
+        $obj->testimonial_subheading = $request->testimonial_subheading;
+        $obj->blog_heading = $request->blog_heading;
+        $obj->blog_subheading = $request->blog_subheading;
+        $obj->save();
 
-        // $obj = HomeItem::where('id', 1)->first();
-
-
-        // return redirect()->back()->with('success', 'Home Items Updated Successfully');
+        return redirect()->back()->with('success', 'Home Items Updated Successfully');
 
     }
 }
