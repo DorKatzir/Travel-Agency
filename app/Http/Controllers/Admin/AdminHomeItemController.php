b1b33b9a179572aaa118101c51a2c1fcb82baf10
Dor Katzir <katzirdor@gmail.com>
2024-10-06 02:34:20 +0300

    Section 33 - Homepage  -  1 / 2

--- app/Http/Controllers/Admin/AdminHomeItemController.php
+++ app/Http/Controllers/Admin/AdminHomeItemController.php
@@ -29,14 +29,32 @@ class AdminHomeItemController extends Controller
         ]);
 
         $obj = HomeItem::where('id', 1)->first();
+
+        if ( $request->hasFile('testimonial_background') ) {
+            $request->validate([
+                'testimonial_background' => 'required|image|mimes:jpeg,jpg,png,gif,svg|max:2048',
+            ]);
+
+            unlink( public_path('uploads/' . $obj->testimonial_background) );
+
+            $final_name = 'testimonial_background_'.time().'.'.$request->testimonial_background->extension();
+            $request->testimonial_background->move( public_path('uploads'), $final_name );
+            $obj->testimonial_background = $final_name;
+        }
+
         $obj->destination_heading = $request->destination_heading;
         $obj->destination_subheading = $request->destination_subheading;
+        $obj->destination_status = $request->destination_status;
+        $obj->feature_status = $request->feature_status;
         $obj->package_heading = $request->package_heading;
         $obj->package_subheading = $request->package_subheading;
+        $obj->package_status = $request->package_status;
         $obj->testimonial_heading = $request->testimonial_heading;
         $obj->testimonial_subheading = $request->testimonial_subheading;
+        $obj->testimonial_status = $request->testimonial_status;
         $obj->blog_heading = $request->blog_heading;
         $obj->blog_subheading = $request->blog_subheading;
+        $obj->blog_status = $request->blog_status;
         $obj->save();
 
         return redirect()->back()->with('success', 'Home Items Updated Successfully');
