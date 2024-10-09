00a26ab83cd7fd7692f434dd13a2caf18fd2704b
Dor Katzir <katzirdor@gmail.com>
2024-10-09 22:29:21 +0300

    Section 34 - Contact Page

--- app/Http/Controllers/Admin/AdminContactItemController.php
+++ app/Http/Controllers/Admin/AdminContactItemController.php
@@ -14,4 +14,12 @@ class AdminContactItemController extends Controller
         $contactItem = ContactItem::where('id', 1)->first();
         return view('admin.contact-item.index', compact('contactItem'));
     }
+
+    public function contactItem_update(Request $request) {
+
+        $obj = ContactItem::where('id', 1)->first();
+        $obj->map_code = $request->map_code;
+        $obj->save();
+        return redirect()->back()->with('success', 'Contact Item Updated Successfully');
+    }
 }
