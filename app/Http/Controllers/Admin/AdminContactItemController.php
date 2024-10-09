2f79193cd7f47b279260a5f6222e95d868b81637
Dor Katzir <katzirdor@gmail.com>
2024-10-09 22:23:06 +0300

    Section 34 - Contact Page

--- app/Http/Controllers/Admin/AdminContactItemController.php
+++ app/Http/Controllers/Admin/AdminContactItemController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\ContactItem;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class AdminContactItemController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $contactItem = ContactItem::where('id', 1)->first();
+        return view('admin.contact-item.index', compact('contactItem'));
+    }
 }
--- resources/views/admin/contact-item/index.blade.php
+++ resources/views/admin/contact-item/index.blade.php
@@ -16,31 +16,26 @@
                 <div class="row">
                     <div class="col-12">
 
-                        <form method="POST" action="{{ route('admin_contactItem_update') }}">
-                            @csrf
-                            {{-- Feature --}}
-                            {{-- <div class="card">
-                                <div class="card-body">
-                                    <h4>Feature Section</h4>
-                                    <div class="mb-3">
-                                        <label class="form-label">Status</label>
-                                        <select name="feature_status" class="form-select">
-                                            <option value="Show" {{ $aboutItem->feature_status == 'Show' ? 'selected' : ''  }}>Show</option>
-                                            <option value="Hide" {{ $aboutItem->feature_status == 'Hide' ? 'selected' : ''  }}>Hide</option>
-                                        </select>
+                        <div class="card">
+                            <div class="card-body">
+                                <form method="POST" action="{{ route('admin_contactItem_update') }}">
+                                    @csrf
+                                    <div class="row mb-4">
+                                        <div class="col-md-12">
+                                            <div>
+                                                <label class="form-label">Map (iframe code)</label>
+                                                <textarea name="map" class="form-control" cols="30" rows="10">{{ $contactItem->map_code }}</textarea>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div> --}}
-                            {{-- End Feature --}}
-                            <div class="card mb-3">
-                                <div class="card-body">
-                                    <div class="d-grid col-4">
+
+                                    <div class="mb-3">
                                         <button type="submit" class="btn btn-primary">Update</button>
                                     </div>
-                                </div>
-                            </div>
 
-                        </form>
+                                </form>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
