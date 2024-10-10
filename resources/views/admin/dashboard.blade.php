a28dcad0f5718053ab09e1af51cf0b16caace675
Dor Katzir <katzirdor@gmail.com>
2024-10-10 19:46:47 +0300

    Section 36 - Admin Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -63,7 +63,7 @@
             <div class="row">
                 <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
-                        <div class="card-icon bg-primary">
+                        <div class="card-icon bg-success">
                             <i class="far fa-user"></i>
                         </div>
                         <div class="card-wrap">
@@ -78,7 +78,7 @@
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
-                        <div class="card-icon bg-danger">
+                        <div class="card-icon bg-info">
                             <i class="fas fa-book-open"></i>
                         </div>
                         <div class="card-wrap">
@@ -111,7 +111,7 @@
             <div class="row">
                 <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
-                        <div class="card-icon bg-danger">
+                        <div class="card-icon bg-primary">
                             <i class="far fa-user"></i>
                         </div>
                         <div class="card-wrap">
@@ -126,7 +126,7 @@
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
-                        <div class="card-icon bg-warning">
+                        <div class="card-icon bg-danger">
                             <i class="fas fa-book-open"></i>
                         </div>
                         <div class="card-wrap">
@@ -141,7 +141,7 @@
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
-                        <div class="card-icon bg-primary">
+                        <div class="card-icon bg-success">
                             <i class="fas fa-bullhorn"></i>
                         </div>
                         <div class="card-wrap">
@@ -159,7 +159,7 @@
             <div class="row">
                 <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
-                        <div class="card-icon bg-primary">
+                        <div class="card-icon bg-info">
                             <i class="far fa-user"></i>
                         </div>
                         <div class="card-wrap">
@@ -174,7 +174,7 @@
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
-                        <div class="card-icon bg-danger">
+                        <div class="card-icon bg-warning">
                             <i class="fas fa-book-open"></i>
                         </div>
                         <div class="card-wrap">
@@ -189,7 +189,7 @@
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
-                        <div class="card-icon bg-warning">
+                        <div class="card-icon bg-primary">
                             <i class="fas fa-bullhorn"></i>
                         </div>
                         <div class="card-wrap">
