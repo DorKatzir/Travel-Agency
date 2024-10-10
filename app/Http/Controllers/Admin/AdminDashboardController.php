09eb9647ca8322481783df0946d6408c78c4caad
Dor Katzir <katzirdor@gmail.com>
2024-10-10 19:37:39 +0300

    Section 36 - Admin Dashboard

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -4,6 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Review;
+use App\Models\Slider;
+use App\Models\Amenity;
+use App\Models\Package;
+use App\Models\Subscriber;
+use App\Models\TeamMember;
+use App\Models\Destination;
+use App\Models\Testimonial;
 use App\Models\BlogCategory;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -11,9 +19,20 @@ use App\Http\Controllers\Controller;
 class AdminDashboardController extends Controller
 {
     public function dashboard() {
-        $total_users = User::get()->count();
-        $total_posts = Post::get()->count();
-        $total_categories = BlogCategory::get()->count();
-        return view('admin.dashboard', compact('total_categories', 'total_posts', 'total_users'));
+
+        $total_testimonials = Testimonial::count();
+        $total_users = User::count();
+        $total_posts = Post::count();
+        $total_categories = BlogCategory::count();
+        $total_team_members = TeamMember::count();
+        $total_sliders = Slider::count();
+        $total_destinations = Destination::count();
+        $total_packages = Package::count();
+        $total_tours = Package::count();
+        $total_revies = Review::count();
+        $total_subscribers = Subscriber::count();
+        $total_amenities = Amenity::count();
+
+        return view('admin.dashboard', compact('total_categories', 'total_posts', 'total_users', 'total_testimonials', 'total_team_members', 'total_sliders', 'total_destinations', 'total_packages', 'total_tours', 'total_revies', 'total_subscribers', 'total_amenities'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,6 +11,55 @@
             <div class="section-header">
                 <h1>Dashboard</h1>
             </div>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-warning">
+                            <i class="fas fa-bullhorn"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Sliders</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ $total_sliders }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-primary">
+                            <i class="far fa-user"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Testimonials</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ $total_testimonials }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-danger">
+                            <i class="fas fa-book-open"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Team Members</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ $total_team_members }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div class="row">
                 <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
@@ -19,7 +68,7 @@
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4>Total News Categories</h4>
+                                <h4>Blog Categories</h4>
                             </div>
                             <div class="card-body">
                                 {{ $total_categories }}
@@ -34,7 +83,7 @@
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4>Total News</h4>
+                                <h4>Blog Posts</h4>
                             </div>
                             <div class="card-body">
                                 {{ $total_posts }}
@@ -49,7 +98,103 @@
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4>Total Users</h4>
+                                <h4>Destinations</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ $total_destinations }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-danger">
+                            <i class="far fa-user"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Packages</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ $total_packages }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-warning">
+                            <i class="fas fa-book-open"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Tours</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ $total_tours }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-primary">
+                            <i class="fas fa-bullhorn"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Amenities</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ $total_amenities }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-primary">
+                            <i class="far fa-user"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Revies</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ $total_revies }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-danger">
+                            <i class="fas fa-book-open"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Subscribers</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ $total_subscribers }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-warning">
+                            <i class="fas fa-bullhorn"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Users</h4>
                             </div>
                             <div class="card-body">
                                 {{ $total_users }}
@@ -58,6 +203,7 @@
                     </div>
                 </div>
             </div>
+
         </section>
     </div>
 
