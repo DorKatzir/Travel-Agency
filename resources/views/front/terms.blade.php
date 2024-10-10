a8a985e683e55952a81c3d556269927255699aab
Dor Katzir <katzirdor@gmail.com>
2024-10-10 16:45:00 +0300

    Section 35 - Privacy and terms Part 2 / 2

---
+++ resources/views/front/terms.blade.php
@@ -0,0 +1,24 @@
+@extends('front.layout.master')
+
+@section('main_content')
+
+    <div class="page-top" style="background-image: url({{ asset('uploads/banner.jpg') }})">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Terms of Use</h2>
+                    <div class="breadcrumb-container">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                            <li class="breadcrumb-item active">Terms of Use</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+@endsection
