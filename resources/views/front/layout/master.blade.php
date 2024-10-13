8425ae05faee29667b682a2f7fd0438fc51c0e1e
Dor Katzir <katzirdor@gmail.com>
2024-10-13 18:43:43 +0300

    Section 38 - Setting - Header & Footer

--- resources/views/front/layout/master.blade.php
+++ resources/views/front/layout/master.blade.php
@@ -52,8 +52,8 @@
                 <div class="row">
                     <div class="col-md-6 left-side">
                         <ul>
-                            <li class="phone-text"><i class="fas fa-phone"></i> 111-222-3333</li>
-                            <li class="email-text"><i class="fas fa-envelope"></i> contact@example.com</li>
+                            <li class="phone-text"><i class="fas fa-phone"></i> {{ $setting->header_phone }}</li>
+                            <li class="email-text"><i class="fas fa-envelope"></i> {{ $setting->header_email }}</li>
                         </ul>
                     </div>
                     <div class="col-md-6 right-side">
