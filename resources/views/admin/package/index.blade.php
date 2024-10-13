c38e142f80563c116d638597de50f5a7bccc3003
Dor Katzir <katzirdor@gmail.com>
2024-10-13 23:48:06 +0300

    Section 39 - Final Things - Adding Demo Data

--- resources/views/admin/package/index.blade.php
+++ resources/views/admin/package/index.blade.php
@@ -43,23 +43,27 @@
                                                     <td>{{ $loop->iteration }}</td>
 
                                                     <td>
-                                                        <img src="{{ asset('uploads/'. $package->featured_photo) }}" alt="" class="h_100 object-cover">
+                                                        <img src="{{ asset('uploads/'. $package->featured_photo) }}" alt="" class="w_200">
                                                     </td>
 
                                                     <td>{{ $package->name }}</td>
 
                                                     <td>
-                                                        <a href="{{ route('admin_package_amenities', $package->id) }}" class="btn btn-success">Amenities</a>
-                                                        <a href="{{ route('admin_package_itineraries', $package->id) }}" class="btn btn-success">Itinerary</a>
-                                                        <a href="{{ route('admin_package_photos', $package->id) }}" class="btn btn-success">Photo gallery</a>
-                                                        <a href="{{ route('admin_package_videos', $package->id) }}" class="btn btn-success">Video gallery</a>
-                                                        <a href="{{ route('admin_package_faqs', $package->id) }}" class="btn btn-success">Faqs</a>
+                                                        <div class="">
+                                                            <a href="{{ route('admin_package_amenities', $package->id) }}" class="btn btn-success mb-2">Amenities</a>
+                                                            <a href="{{ route('admin_package_itineraries', $package->id) }}" class="btn btn-success mb-2">Itinerary</a>
+                                                            <a href="{{ route('admin_package_faqs', $package->id) }}" class="btn btn-success mb-2">Faqs</a>
+                                                        </div>
+                                                        <div class="">
+                                                            <a href="{{ route('admin_package_photos', $package->id) }}" class="btn btn-success mb-2">Photo gallery</a>
+                                                            <a href="{{ route('admin_package_videos', $package->id) }}" class="btn btn-success mb-2">Video gallery</a>
+                                                        </div>
 
                                                     </td>
 
                                                     <td class="pt_10 pb_10">
-                                                        <a href="{{ route('admin_package_edit', $package->id) }}" class="btn btn-primary"><i class="fas fa-edit"></i></a>
-                                                        <a href="{{ route('admin_package_delete', $package->id) }}" class="btn btn-danger" onClick="return confirm('Are you sure?');"><i class="fas fa-trash"></i></a>
+                                                        <a href="{{ route('admin_package_edit', $package->id) }}" class="btn btn-primary mb-2"><i class="fas fa-edit"></i></a>
+                                                        <a href="{{ route('admin_package_delete', $package->id) }}" class="btn btn-danger mb-2" onClick="return confirm('Are you sure?');"><i class="fas fa-trash"></i></a>
                                                     </td>
                                                 </tr>
 
